417c7d67dab3241065e5529d8c95904459c20e04
JanKarlicek22 <119809083+JanKarlicek22@users.noreply.github.com>
2022-12-05 21:39:22 +0100

    Update streamlit_app.py
    Added new variables for selected fruits and fruits to show - filtering the source dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,9 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 # Choose the Fruit Name Column as the Index
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include (with pre-selected values)
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+# Let's put a pick list here so they can pick the fruit they want to include (with pre-selected values) - save it as variable and use this variable to filter the dataframe
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
